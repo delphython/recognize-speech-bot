6bd62ae6ea5cef10db2691279cbaab12b1ffca5c
delphython <delphython@gmail.com>
2022-02-06 13:11:39 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import (
     Updater,
@@ -11,12 +12,54 @@ from telegram.ext import (
     CallbackContext,
 )
 
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "C:\\Users\\kvv\\python\\recognize-speech-bot\\pelagic-berm-340508-a69ce9c87cda.json"
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
 logger = logging.getLogger(__name__)
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code="en-US"):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    # print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(
+            text=text, language_code=language_code
+        )
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        # print("=" * 20)
+        # print("Query text: {}".format(response.query_result.query_text))
+        # print(
+        #     "Detected intent: {} (confidence: {})\n".format(
+        #         response.query_result.intent.display_name,
+        #         response.query_result.intent_detection_confidence,
+        #     )
+        # )
+        # print(
+        #     "Fulfillment text: {}\n".format(
+        #         response.query_result.fulfillment_text
+        #     )
+        # )
+        return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -35,6 +78,9 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
+    reply_message = detect_intent_texts(
+        project_id, sesion_id, update.message.text
+    )
     update.message.reply_text(update.message.text)
 
 
@@ -42,6 +88,11 @@ def main() -> None:
     """Start the bot."""
     load_dotenv()
 
+    global project_id
+    global sesion_id
+
+    project_id = os.getenv("PROJECT_ID")
+    sesion_id = os.getenv("SESSION_ID")
     telegram_token = os.getenv("TELEGRAM_TOKEN")
 
     updater = Updater(telegram_token)
