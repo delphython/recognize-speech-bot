9267f9691b7ca88a4eedf605b50f50b681f62dbf
delphython <delphython@gmail.com>
2022-02-08 12:12:22 +0300

    Add monitoring

--- vk_bot.py
+++ vk_bot.py
@@ -6,15 +6,22 @@ import vk_api as vk
 
 from dotenv import load_dotenv
 from google.cloud import dialogflow
+from telegram import Bot
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
 logger = logging.getLogger(__name__)
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code="ru"):
@@ -50,19 +57,31 @@ def main():
     vk_token = os.getenv("VK_TOKEN")
     project_id = os.getenv("PROJECT_ID")
     sesion_id = os.getenv("SESSION_ID")
+    chat_id = os.getenv("CHAT_ID")
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+
+    bot = Bot(token=telegram_token)
+
+    logging.basicConfig(format="%(levelname)s %(message)s")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    logger.info("Бот запущен!")
 
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
 
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            reply_message = detect_intent_texts(
-                project_id, sesion_id, event.text
-            )
-            vk_api.messages.send(
-                user_id=event.user_id, message=reply_message, random_id=0
-            )
+    try:
+        longpoll = VkLongPoll(vk_session)
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                reply_message = detect_intent_texts(
+                    project_id, sesion_id, event.text
+                )
+                vk_api.messages.send(
+                    user_id=event.user_id, message=reply_message, random_id=0
+                )
+    except Exception as err:
+        logger.exception(f"Бот упал с ошибкой: {err}")
 
 
 if __name__ == "__main__":
