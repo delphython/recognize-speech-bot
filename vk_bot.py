c007936050d0c526915c0f7b2aed0a8c11090d3c
delphython <delphython@gmail.com>
2022-02-08 09:22:45 +0300

    Add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -4,7 +4,30 @@ import random
 import vk_api as vk
 
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def detect_intent_texts(project_id, session_id, text, language_code="ru"):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    # for text in texts:
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
 
 
 def echo(event, vk_api):
@@ -18,7 +41,12 @@ def echo(event, vk_api):
 def main():
     load_dotenv()
 
+    google_app_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_app_credentials
+
     vk_token = os.getenv("VK_TOKEN")
+    project_id = os.getenv("PROJECT_ID")
+    sesion_id = os.getenv("SESSION_ID")
 
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
@@ -26,7 +54,12 @@ def main():
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply_message = detect_intent_texts(
+                project_id, sesion_id, event.text
+            )
+            vk_api.messages.send(
+                user_id=event.user_id, message=reply_message, random_id=0
+            )
 
 
 if __name__ == "__main__":
